ec0c1c935d2d61a468a108bf822f7aadedd1f028
Princy Patel <kapatelprincy@gmail.com>
2024-08-07 22:05:36 +0530

    feat:add hero section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
+import Hero from '../../components/Hero';
 
 function HomePage() {
   return (
     <div>
-      <Header />
       {/* Adjust the margin to accommodate the sidebar width */}
       <main style={{ marginLeft: 250, padding: '20px' }}>
         <Sidebar />
+        <Hero />
       </main>
     </div>
   );
