6583deb0629c33cd464629b86d84e0a27ba20675
Princy Patel <kapatelprincy@gmail.com>
2024-09-21 14:26:40 +0530

    feat:validation with formik

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,92 +3,103 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {'Copyright © '}
-      eCommerce
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+const signUpSchema = Yup.object().shape({
+  userName: Yup.string().required('Username is required'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(6, 'must be of 6 character long'),
+});
 
 function Login() {
   const defaultTheme = createTheme();
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div className="bg-login-bg h-screen bg-cover bg-center flex items-center justify-center">
-        <div className="bg-gray-200 w-full max-w-md p-8 rounded-lg shadow-lg">
-          <div className="flex items-center justify-center mb-4">
-            <Avatar className="m-2 bg-black">
+      <div className="bg-login-bg flex h-screen items-center justify-center bg-cover bg-center">
+        <div className="w-full max-w-md rounded-lg bg-gray-200 p-8 shadow-lg">
+          <div className="mb-4 flex items-center justify-center">
+            <Avatar sx={{ bgcolor: 'black' }}>
               <LockOutlinedIcon />
             </Avatar>
           </div>
-          <Typography component="h1" variant="h5" className="text-center mb-6">
+          <Typography component="h1" variant="h5" className="!my-3 text-center">
             Login to your account
           </Typography>
-          <Box component="form" noValidate>
-            <TextField
-              required
-              fullWidth
-              name="userName"
-              label="Username"
-              id="userName"
-              autoComplete="given-name"
-              sx={{ marginBottom: 2 }}
-            />
-            <TextField
-              name="password"
-              required
-              fullWidth
-              id="password"
-              label="Password"
-              autoComplete="new-password"
-              type="password"
-              sx={{ marginBottom: 2 }}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2, backgroundColor: 'black' }}
-
-            >
-              Log In
-            </Button>
-            <Grid container>
-
-              <Grid item xs>
-                <Link href="#" variant="body2" sx={{ color: 'black' }}>
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/" variant="body2" sx={{ color: 'black' }}>
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-          <Copyright sx={{ mt: 8, mb: 4 }} />
+          <Formik
+            initialValues={{ userName: '', password: '' }}
+            validationSchema={signUpSchema}
+          >
+            {(formik) => (
+              <Box component="form" noValidate onSubmit={formik.handleSubmit}>
+                <TextField
+                  {...formik.getFieldProps('userName')}
+                  error={
+                    formik.touched.userName && Boolean(formik.errors.userName)
+                  }
+                  required
+                  fullWidth
+                  name="userName"
+                  label="Username"
+                  id="userName"
+                  autoComplete="given-name"
+                  sx={{ marginBottom: 2 }}
+                  helperText={formik.touched.userName && formik.errors.userName}
+                />
+                <TextField
+                  {...formik.getFieldProps('password')}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  name="password"
+                  required
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  autoComplete="new-password"
+                  type="password"
+                  sx={{ marginBottom: 2 }}
+                  helperText={formik.touched.password && formik.errors.password}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, backgroundColor: 'black' }}
+                >
+                  Log In
+                </Button>
+                <Grid container>
+                  <Grid item xs className="cursor-not-allowed opacity-30">
+                    <Link to="#" variant="body2" sx={{ color: 'black' }}>
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link
+                      to="/signup"
+                      variant="body2"
+                      className="text-gray-500 underline"
+                    >
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            )}
+          </Formik>
         </div>
       </div>
     </ThemeProvider>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,52 +4,44 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        eCommerce
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { FormHelperText } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const defaultTheme = createTheme();
 
-function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+const signUpSchema = Yup.object().shape({
+  firstName: Yup.string().required('First name is required'),
+  lastName: Yup.string().required('Last name is required'),
+  email: Yup.string().email().required('Email id required'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(6, 'must be of 6 character long'),
+  termsAndCondition: Yup.bool().oneOf(
+    [true],
+    'No terms, no threads! Fashion needs rules—please accept!',
+  ),
+});
 
+function SignUp() {
   return (
-    <ThemeProvider theme={defaultTheme} >
-      <div className='bg-login-bg flex items-center justify-center'>
-        <Container component="main" maxWidth="xs" sx={{
-          border: "2px solid",
-          margin: "5% auto"
-        }} className='bg-gray-200 p-8 rounded-lg shadow-lg '>
+    <ThemeProvider theme={defaultTheme}>
+      <div className="bg-login-bg flex items-center justify-center">
+        <Container
+          component="main"
+          maxWidth="xs"
+          sx={{
+            margin: '5% auto',
+          }}
+          className="rounded-lg bg-gray-200 p-8 shadow-lg"
+        >
           <CssBaseline />
           <Box
             sx={{
@@ -57,83 +49,160 @@ function SignUp() {
               flexDirection: 'column',
               alignItems: 'center',
             }}
-
           >
-            <Avatar sx={{ m: 1, bgcolor: 'black' }}>
+            <Avatar sx={{ bgcolor: 'black' }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
               Sign up
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete="given-name"
-                    name="firstName"
-                    required
+
+            <Formik
+              initialValues={{
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: '',
+                termsAndCondition: false,
+              }}
+              validationSchema={signUpSchema}
+            >
+              {(formik) => (
+                <Box
+                  component="form"
+                  noValidate
+                  onSubmit={formik.handleSubmit}
+                  sx={{ mt: 3 }}
+                >
+                  {console.log(formik)}
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        {...formik.getFieldProps('firstName')}
+                        error={
+                          formik.touched.firstName &&
+                          Boolean(formik.errors.firstName)
+                        }
+                        autoComplete="given-name"
+                        name="firstName"
+                        required
+                        fullWidth
+                        id="firstName"
+                        label="First Name"
+                        autoFocus
+                        helperText={
+                          formik.touched.firstName && formik.errors.firstName
+                        }
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        {...formik.getFieldProps('lastName')}
+                        error={
+                          formik.touched.lastName &&
+                          Boolean(formik.errors.lastName)
+                        }
+                        required
+                        fullWidth
+                        id="lastName"
+                        label="Last Name"
+                        name="lastName"
+                        autoComplete="family-name"
+                        helperText={
+                          formik.touched.lastName && formik.errors.lastName
+                        }
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        {...formik.getFieldProps('email')}
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        error={
+                          formik.touched.email && Boolean(formik.errors.email)
+                        }
+                        helperText={formik.touched.email && formik.errors.email}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        {...formik.getFieldProps('password')}
+                        error={
+                          formik.touched.password &&
+                          Boolean(formik.errors.password)
+                        }
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                        helperText={
+                          formik.touched.password && formik.errors.password
+                        }
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            required
+                            {...formik.getFieldProps('termsAndCondition')}
+                            value="termsAndCondition"
+                            color="primary"
+                            name="termsAndCondition"
+                            id="termsAndCondition"
+                            error={
+                              formik.touched.termsAndCondition &&
+                              Boolean(formik.errors.termsAndCondition)
+                            }
+                          />
+                        }
+                        label={
+                          <>
+                            I agree to the{' '}
+                            <Link to="/terms-and-conditions">
+                              Terms and Conditions
+                            </Link>
+                          </>
+                        }
+                      />
+                      {formik.touched.termsAndCondition &&
+                        formik.errors.termsAndCondition && (
+                          <FormHelperText error>
+                            {formik.errors.termsAndCondition}
+                          </FormHelperText>
+                        )}
+                    </Grid>
+                  </Grid>
+                  <Button
+                    type="submit"
                     fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="family-name"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControlLabel
-                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                    label="I agree to the terms and conditions."
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2, backgroundColor: "black" }}
-              >
-                Sign Up
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link href="#" variant="body2" sx={{ color: "black" }}>
-                    Already have an account? Sign in
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2, backgroundColor: 'black' }}
+                  >
+                    Sign Up
+                  </Button>
+                  <Grid container justifyContent="flex-end">
+                    <Grid item>
+                      <Link
+                        to="/login"
+                        variant="body2"
+                        className="text-gray-500 underline"
+                      >
+                        Already have an account? Login
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+              )}
+            </Formik>
           </Box>
-          <Copyright sx={{ mt: 5 }} />
         </Container>
       </div>
     </ThemeProvider>
