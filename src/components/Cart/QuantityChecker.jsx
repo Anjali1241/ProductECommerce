538c375b61586629e3f07de1160b3f7e07fd01b4
Anjali Sharma <sharmaanjali1241@gmail.com>
2024-10-22 23:07:28 +0530

    design checkout page

---
+++ src/components/Cart/QuantityChecker.jsx
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { Unstable_NumberInput as BaseNumberInput } from '@mui/base/Unstable_NumberInput';
+import clsx from 'clsx';
+
+export default function NumberInputIntroduction() {
+  const [value, setValue] = React.useState(null);
+  return (
+    <NumberInput
+      aria-label="Demo number input"
+      placeholder="Num" // Shorter placeholder text
+      value={value}
+      onChange={(event, val) => setValue(val)}
+    />
+  );
+}
+
+const resolveSlotProps = (fn, args) => (typeof fn === 'function' ? fn(args) : fn);
+
+const NumberInput = React.forwardRef(function NumberInput(props, ref) {
+  return (
+    <BaseNumberInput
+      className="w-60" // Keep the width small
+      {...props}
+      ref={ref}
+      slotProps={{
+        root: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.root,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              'grid grid-cols-[1fr_8px] grid-rows-2 overflow-hidden font-sans rounded-lg  dark:text-slate-300 border border-solid bg-transparent dark:bg-transparent hover:border-violet-400 dark:hover:border-violet-400 focus-visible:outline-0 p-1',
+              ownerState.focused
+                ? 'border-violet-400 dark:border-violet-400 shadow-lg shadow-outline-purple dark:shadow-outline-purple'
+                : 'border-slate-300 dark:border-slate-600 shadow-md shadow-slate-100 dark:shadow-slate-400',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+        input: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.input,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            className: clsx(
+              'col-start-1 col-end-2 row-start-1 row-end-3 text-sm font-sans leading-normal text-slate-900 bg-transparent dark:bg-transparent border-0 rounded-[inherit] dark:text-slate-900 px-1 py-1 outline-0 focus-visible:outline-0 focus-visible:outline-none', // Reduced padding
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+        incrementButton: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.incrementButton,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            children: '▴',
+            className: clsx(
+              'font-[system-ui] flex flex-row flex-nowrap justify-center items-center p-0 w-5 h-5 border border-solid outline-none text-xs box-border leading-[1.2] rounded-t-md border-slate-200 bg-transparent dark:bg-transparent text-slate-900 dark:text-slate-300 transition-all duration-[120ms] hover:cursor-pointer hover:bg-purple-500 hover:text-slate-50 dark:hover:bg-slate-800 dark:border-slate-600 col-start-3 col-end-3 row-start-1 row-end-2',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+        decrementButton: (ownerState) => {
+          const resolvedSlotProps = resolveSlotProps(
+            props.slotProps?.decrementButton,
+            ownerState,
+          );
+          return {
+            ...resolvedSlotProps,
+            children: '▾',
+            className: clsx(
+              'font-[system-ui] flex flex-row flex-nowrap justify-center items-center p-0 w-5 h-5 border border-solid outline-none text-xs box-border leading-[1.2] rounded-b-md border-slate-200 bg-transparent dark:bg-transparent text-slate-900 dark:text-slate-300 transition-all duration-[120ms] hover:cursor-pointer hover:bg-purple-500 hover:text-slate-50 dark:hover:bg-slate-800 dark:border-slate-600 col-start-3 col-end-3 row-start-2 row-end-3',
+              resolvedSlotProps?.className,
+            ),
+          };
+        },
+      }}
+    />
+  );
+});
+
+NumberInput.propTypes = {
+  /**
+   * The props used for each slot inside the NumberInput.
+   * @default {}
+   */
+  slotProps: PropTypes.shape({
+    decrementButton: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+    incrementButton: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+    input: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+    root: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+  }),
+};
--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -9,6 +9,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import NumberInputIntroduction from './QuantityChecker';
 
 function handleClick(event) {
   event.preventDefault();
@@ -19,11 +20,11 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, "$24"),
+  createData('Ice cream sandwich', 237, 9.0, "$37"),
+  createData('Eclair', 262, 16.0, "$24"),
+  createData('Cupcake', 305, 3.7, "$67"),
+  createData('Gingerbread', 356, 16.0, "$49"),
 ];
 
 function Cart() {
@@ -36,14 +37,14 @@ function Cart() {
 
         <Typography sx={{ color: 'text.primary' }}>Cart</Typography>
       </Breadcrumbs>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <TableContainer component={Paper} className="my-10 w-7">
+        <Table aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Product (100g serving)</TableCell>
-              <TableCell align="right">Price</TableCell>
-              <TableCell align="right">Quantity&nbsp;(g)</TableCell>
-              <TableCell align="right">Subtotal&nbsp;(g)</TableCell>
+              <TableCell align="left">Product</TableCell>
+              <TableCell align="left">Price</TableCell>
+              <TableCell align="left">Quantity</TableCell>
+              <TableCell align="left">Subtotal</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -52,13 +53,15 @@ function Cart() {
                 key={row.name}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
-                <TableCell component="th" scope="row">
+                <TableCell component="th" scope="row" className="w-64">
                   {row.name}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="left" className="w-64">{row.calories}</TableCell>
+                <TableCell align="left" className="w-64">
+                  <NumberInputIntroduction />
+                </TableCell>
+                <TableCell align="left" className="w-64">{row.carbs}</TableCell>
+                {/* <TableCell align="right">{row.protein}</TableCell> */}
               </TableRow>
             ))}
           </TableBody>
